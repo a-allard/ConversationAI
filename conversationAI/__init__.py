d910c544d2c310b8eeab615fcc1a0a975a9b03b0
Allard <skunk345678hi@gmail.com>
2017-10-14 20:12:19 -0400

    adding input processing class.  Note nothing is working yet.  This is still in the theoretical process.

--- conversationAI/__init__.py
+++ conversationAI/__init__.py
@@ -12,6 +12,13 @@
 
 class processType:
 
-    def __init__():
+    _types=('Question','Statement')
+    _StatementTypes('Objective','Subjective','Argumentative','Theorizing','Definative')
+
+    def __init__(statemnet):
+        assert isinstance(statement,str)
+        self.statement=statement
         pass
-    
+
+    def isQuestion(self):
+        
