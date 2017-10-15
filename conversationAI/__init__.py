71eeb536d2e7722b425c841c3fb4e1c1cd7acc1e
Allard <skunk345678hi@gmail.com>
2017-10-14 20:18:20 -0400

    Adding a class to hold the conversation.

--- conversationAI/__init__.py
+++ conversationAI/__init__.py
@@ -5,6 +5,12 @@
 
 
 
+class conversation:
+    def __init__(self):
+        self._typesOfConversation=('General','Argument','Debate','Comfort','Playful','Punishment')
+        self._CurrentConversationType=self._typeOfConversation(0)
+        self._PreviousTypesOfConversations=None
+
 
 
 
@@ -15,10 +21,13 @@ class processType:
     _types=('Question','Statement')
     _StatementTypes('Objective','Subjective','Argumentative','Theorizing','Definative')
 
-    def __init__(statemnet):
-        assert isinstance(statement,str)
-        self.statement=statement
-        pass
+    def __init__(self,stringIn):
+        assert isinstance(stringIn,str)
+        self.stringToPrcoess=stringIn
+        self.isQuestion=None
+        self.isArgument=None
 
     def isQuestion(self):
-        
+        if(self.stringToPrcoess.endswith('?')):
+            self.isQuestion=True    #If the user was kind enough to put a ? on the end of the input we can easily decide this is a question.  Not always true but lets assume it is true for now.
+            
